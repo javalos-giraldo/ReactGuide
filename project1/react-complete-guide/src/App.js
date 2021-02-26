aa0dd74f7a319ea4d7f8f82e92060924e5b2f5c0
javalos <156javalos@gmail.com>
2021-02-26 12:50:03 -0800

    new values

--- project1/react-complete-guide/src/App.js
+++ project1/react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component , useState} from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -31,65 +31,121 @@ import Person from './Person/Person';
 //     });
 //     }
 
+
 class App extends Component {
 
   state = {
     persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age:29},
-      {name: 'Stephanie', age:26}
+      {id: 'qwewq', name: 'Max', age: 28},
+      {id: 'asdas', name: 'Manu', age:29},
+      {id: 'assss', name: 'Stephanie', age:26}
     ],
-     otherState: 'some other value'
+     otherState: 'some other value',
+     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({persons: [
-      {name: newName, age: 28},
-      {name: 'Manu', age:29},
-      {name: 'Stephanie', age:27}
-    ]})
-    //console.log('HELLO WORLD');
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();// gets you a copy of the array when no args
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (id,event) => {
+    const personIndex = this.state.persons.findIndex(
+      p => {
+        return p.id === id;
+      }
+    )
+
+    // Spread operators to copy all the properties
+    // rather than using the object itself
+    const person = {...this.state.persons[personIndex]};
+    //OR
+    // const person = Object.assing({}, this.state.persons[personIndex]);
+
+    // working only with copies instead of the objects themselves
+
+    person.name  = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
     this.setState({
-      persons: [
-      {name: 'Manu', age: 28},
-      {name: event.target.value, age:29},
-      {name: 'Stephanie', age:26}
-    ]})
+      persons: persons})
+  }
+
+  togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
   }
 
     // Bind over setting up as a function
     render() {
       //inline styles
       const style= {
-        backgroundColor: 'white',
+        backgroundColor: 'green',
+        color: 'white',
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover' : {
+          backGroundColor: 'lightgreen',
+          color: 'black'
+        }
       }
+
+      let persons = null;
+
+      if(this.state.showPersons){
+        persons = (
+          <div>
+              {this.state.persons.map((person, index) => {
+                return(
+                  <Person
+                    name = {person.name}
+                    age = {person.age}
+                    click = {() => {this.deletePersonHandler(index)}}
+                    //Keys make it more efficient so that the DOM
+                    //recognizes which element to update
+                    key={person.id}
+                    changed={this.nameChangedHandler.bind(this,person.id)} />
+                    
+                );
+              })}
+          </div>
+        );
+
+        style.backgroundColor = 'red';
+        style[':hover'] = {
+            backgroundColor: 'lightred',
+            color: 'black'
+        }
+      }
+
+      const classes = [];
+      if(this.state.persons.length <= 2){
+        classes.push('red'); // classes = ['red']
+      }
+
+      if(this.state.persons.length <= 1){
+        classes.push('bold'); // classes = ['red','bold']
+      }
+
+
+
 
       return (
         <div className="App">
           <h1>Hi, I'm a React App.</h1>
-          <p> This is really working!</p>
-          <button
-            style={style}
-            onClick={() => this.switchNameHandler('Maximiliam!!')}>Switch Name</button>
-          <Person 
-            name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person 
-            name ={this.state.persons[1].name} age= {this.state.persons[1].age} 
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}>
-              My Hobbies: Racing</Person>
-          <Person 
-            name = {this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <p className={classes.join(' ')}> This is really working!</p>
+          <button className="button"
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+            {persons}
         </div>
-      );
-    };
+      )
+    }
     //return React.createElement('div', {className: 'App'},React.createElement('h1',null,'Does this work now?'));
   
 }
